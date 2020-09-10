244076d9c05967216920380ddae8f1282ba79f80
AndyTraficante <andyt.0814@gmail.com>
2020-09-10 09:01:40 -0400

    am i doing this right

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,5 +1,60 @@
 package com.company;
 
 public class LinkedList {
+    Node head;
+    Node tail;
 
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+
+    public class Node {
+        Object data;
+        Node next;
+        Node previous;
+
+        public Node(Object data) {
+            this.data = data;
+            next = null;
+        }
+
+
+        public Object getData() {
+            return data;
+        }
+
+        public void setData(Object data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
+
+        Node node1 = new Node("FIRST!");
+
+        public boolean add(Object item) {
+            Node node = new Node(item);
+            this.setPrevious(tail);
+            node = tail;
+            return true;
+
+
+        }
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	LinkedList mylist = new LinkedList();
+
+	mylist.add("hello")
+
+
     }
 }
